5d64e49fa30e837ae31c9dc4e88e37dd6041ab3e
Tim Denison <tim.denison@gmail.com>
2023-03-19 15:01:28 -0700

    initial commit of working notes app

---
+++ GiftIt/Views/About.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace GiftIt.Views;
+
+public partial class About : ContentPage
+{
+	public About()
+	{
+		InitializeComponent();
+	}
+
+    private async void  LearnMore_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
